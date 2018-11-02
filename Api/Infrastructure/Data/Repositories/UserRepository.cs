ebe7fe3285db161d1fc1363792c01ac93b8a8ebb
Sam Miller <sithpow@gmail.com>
2018-11-01 21:24:59 -0500

    Fill out UserRepository

--- Api/Infrastructure/Data/Repositories/UserRepository.cs
+++ Api/Infrastructure/Data/Repositories/UserRepository.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
 using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SamMiller.Mumba.Api.Infrastructure.Identity;
+using SamMiller.Mumba.doApi.Core.Entities;
 
 namespace SamMiller.Mumba.Api.Infrastructure.Data.Repositories
 {
@@ -15,12 +19,58 @@ namespace SamMiller.Mumba.Api.Infrastructure.Data.Repositories
             _appDbContext = appDbContext;
         }
 
-        public async Task CreateAsync(string username, string password){
+        public async Task CreateAsync(string username, string password)
+        {
             Guard.Against.Null(username, nameof(username));
             Guard.Against.Null(password, nameof(password));
 
+            var NewAppUser = new AppUser
+            {
+                UserName = username
+            };
 
+            var createUserResult = await _userManager.CreateAsync(NewAppUser, password);
+
+            if (!createUserResult.Succeeded)
+            {
+                throw new CreateUserException(createUserResult);
+            }
+
+            var NewUser = new User
+            {
+                username = username
+            };
+
+            _appDbContext.Users.Add(NewUser);
+
+            try
+            {
+                await _appDbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var AppUser = await _userManager.FindByNameAsync(username);
+                await _userManager.DeleteAsync(AppUser);
+
+                throw new CreateUserException($"The username '{username}' is already in use.", exception);
+            }
 
         }
+    }
+
+    [Serializable]
+    internal class CreateUserException : Exception
+    {
+        public CreateUserException(IdentityResult createUserResult) : this(createUserResult.Errors.Select(error => $"Code: {error.Code}; Description: {error.Description}.").Aggregate((error1, error2) => error1 + "\n" + error2))
+        { }
+
+        public CreateUserException(string message) : base(message)
+        { }
+
+        public CreateUserException(string message, Exception innerException) : base(message, innerException)
+        { }
+
+        protected CreateUserException(SerializationInfo info, StreamingContext context) : base(info, context)
+        { }
     }
 }
