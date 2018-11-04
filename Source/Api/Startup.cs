d6e89702becd82cf40c09875eef091c146e4e3ad
Sam Miller <sithpow@gmail.com>
2018-11-04 16:30:33 -0600

    Formating and enable Swagger

--- Source/Api/Startup.cs
+++ Source/Api/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
 using Microsoft.Extensions.DependencyInjection;
 using SamMiller.Mumba.Api.Infrastructure.Identity;
 
@@ -15,32 +16,47 @@ namespace SamMiller.Mumba.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<AppIdentityDbContext>(options => {
-                options.UseInMemoryDatabase(nameof(AppIdentityDbContext));
+            services.AddDbContext<IdentityDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(nameof(IdentityDbContext));
             });
 
-            services.AddIdentityCore<AppUser>(options =>{
+            services.AddIdentityCore<IdentityUser>(options =>
+            {
                 options.Password.RequireDigit = false;
                 options.Password.RequiredLength = 8;
-                options.Password.RequiredUniqueChars= 0;
-                options.Password.RequireLowercase=false;
-                options.Password.RequireNonAlphanumeric=false;
-                options.Password.RequireUppercase=false;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
 
+            });
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else{
-            app.UseHsts();
-            app.UseHttpsRedirection();
+            else
+            {
+                app.UseHsts();
+                app.UseHttpsRedirection();
             }
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
