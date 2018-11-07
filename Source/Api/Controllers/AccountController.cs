6aed599bb04e4f72c6d238b7bddacb1702392c01
Sam Miller <sithpow@gmail.com>
2018-11-06 19:08:05 -0600

    Drop most of ApiController for now

--- Source/Api/Controllers/AccountController.cs
+++ Source/Api/Controllers/AccountController.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SamMiller.Mumba.Api.Infrastructure.Data.Repositories;
+using SamMiller.Mumba.Api.Infrastructure.Data;
 using SamMiller.Mumba.Api.Models.Requests;
 
 namespace SamMiller.Mumba.Api.Controllers
 {
+
     [ApiController]
     [Route("api/account")]
     public class AccountController : ApiControllerBase
@@ -16,23 +17,10 @@ namespace SamMiller.Mumba.Api.Controllers
         /// <param name="registerUserRequest"></param>
         /// <param name="userRepository"></param>
         /// <returns></returns>
-        [HttpPost]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(typeof(string), 400)]
-        public async Task<IActionResult> RegisterUserAsync([FromBody] RegisterUserRequest registerUserRequest, [FromServices] UserRepository userRepository)
-        {
-            try
-            {
-                await userRepository.CreateAsync(registerUserRequest.Username, registerUserRequest.Password);
-
-                return Ok();
-            }
-            catch (CreateUserException exception)
-            {
-                return BadRequest(exception.Message);
-            }
-        }
-
+        // [HttpPost]
+        // [ProducesResponseType(200)]
+        // [ProducesResponseType(typeof(string), 400)]
+        
 
     }
 }
