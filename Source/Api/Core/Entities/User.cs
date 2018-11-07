e459e5a4b111693284e28c622b25955835e2d112
Sam Miller <sithpow@gmail.com>
2018-11-06 19:07:22 -0600

    update User class

--- Source/Api/Core/Entities/User.cs
+++ Source/Api/Core/Entities/User.cs
@@ -1,19 +1,8 @@
 using System;
-
+using Microsoft.AspNetCore.Identity;
 
 namespace SamMiller.Mumba.Api.Core.Entities
 {
-    public class User : IEntity<Guid>
-    {
-        public Guid Id { get; private set; }
-        public string Username { get; private set; }
-        public string Password { get; private set; }
-        public User(Guid id, string username, string password)
-        {
-            Id = id;
-            Username = username;
-            Password = password;
-        }
-
-    }
+    public class User : IdentityUser<Guid>, IEntity<Guid>
+    { }
 }
