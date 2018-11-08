94ac54cec9d1e668cecdce2fe6ab5837429a26a2
Sam Miller <sithpow@gmail.com>
2018-11-08 16:45:45 -0600

    add board and task controllers

---
+++ Source/Api/Controllers/BoardController.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SamMiller.Mumba.Api.Infrastructure.Data;
+using SamMiller.Mumba.Api.Models.Requests;
+
+namespace SamMiller.Mumba.Api.Controllers
+{
+
+    /// <summary>
+    /// The controller for boards
+    /// </summary>
+    [ApiController]
+    [Route("api/board")]
+    
+    public class BoardController : ApiControllerBase
+    { }
+}
---
+++ Source/Api/Controllers/TaskController.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SamMiller.Mumba.Api.Infrastructure.Data;
+using SamMiller.Mumba.Api.Models.Requests;
+
+namespace SamMiller.Mumba.Api.Controllers
+{
+
+    /// <summary>
+    /// The controller for Tasks
+    /// </summary>
+    [ApiController]
+    [Route("api/task")]
+    public class TaskController : ApiControllerBase
+    { }
+}
