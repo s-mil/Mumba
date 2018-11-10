bfec223144215106fc9eceb95ee02e9415ade554
Sam Miller <sithpow@gmail.com>
2018-11-10 17:32:15 -0600

    add DB context

---
+++ Src/Data/MumbaContext.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SamMiller.Mumba.Data
+{
+    /// <summary>
+    /// The context for the GoldenTicket Api
+    /// </summary>
+    public class MumbaContext : IdentityDbContext<AppUser>
+    {
+        /// <summary>
+        /// The collection of Users
+        /// </summary>
+        public DbSet<AppUser> Users { get; set; }
+
+        /// <summary>
+        /// The collection of boards
+        /// </summary>
+        public DbSet<Board> Boards { get; set; }
+
+        /// <summary>
+        /// The collection of tasks
+        /// </summary>
+        public DbSet<Task> Tasks { get; set; }
+
+
+        /// <summary>
+        /// The constructor for this context
+        /// </summary>
+        /// <param name="options">The options to create the context around</param>
+        /// <returns>A new instance of this context</returns>
+        public MumbaContext(DbContextOptions<MumbaContext> options) : base(options)
+        { }
+    }
+}
