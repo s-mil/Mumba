e106d6a85ff45a1728d5f77e312c46db02ae1f88
Sam Miller <sithpow@gmail.com>
2018-11-11 13:39:14 -0600

    Add board view

---
+++ Src/Models/BoardViewModels/BoardView.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using SamMiller.Mumba.Models;
+
+namespace SamMiller.Mumba.Models.BoardViewModels
+{
+    public class BoardView
+    {
+        /// <summary>
+        /// Gets User
+        /// </summary>
+        public AppUser user { get; set; }
+
+        
+        public Board board {get; set;}
+
+        /// <summary>
+        /// List of Tasks associated with the Board
+        /// </summary>
+        /// <returns>Boards Tasks</returns>
+        public IEnumerable<Task> Tasks { get; set; }
+    }
+}
