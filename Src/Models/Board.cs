4e540b8eaae8556b69a57793430ead994fe440d1
Sam Miller <sithpow@gmail.com>
2018-11-11 13:33:45 -0600

    Add Board model

---
+++ Src/Models/Board.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SamMiller.Mumba.Models
+{
+    /// <summary>
+    /// A board
+    /// </summary>
+    public class Board
+    {
+        /// <summary>
+        /// The Id for this board
+        /// </summary>
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The ID of the owner of the board
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// The title for the board
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
