2e0849c7d3261c2fb2f3bfba07b984a5cceaec91
Sam Miller <sithpow@gmail.com>
2018-11-11 13:37:54 -0600

    Create User boards view model

---
+++ Src/Models/BoardViewModels/BoardsView.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using SamMiller.Mumba.Models;
+
+namespace SamMiller.Mumba.Models.BoardViewModels
+{
+    public class BoardsView
+    {
+        /// <summary>
+        /// Gets User
+        /// </summary>
+        public AppUser user { get; set; }
+
+        /// <summary>
+        /// List of Boards associated with the user
+        /// </summary>
+        /// <returns>Users boards</returns>
+        public IEnumerable<Board> Boards { get; set; }
+    }
+}
