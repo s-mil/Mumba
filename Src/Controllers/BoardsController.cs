bcaa69d6e492945104f85c070da6627cd5cfee54
Sam Miller <sithpow@gmail.com>
2018-11-16 00:32:25 -0600

    Begin board open process

--- Src/Controllers/BoardsController.cs
+++ Src/Controllers/BoardsController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using SamMiller.Mumba.Models.BoardViewModels;
 using System;
 using System.Linq;
+using System.Collections;
 
 namespace SamMiller.Mumba.Controllers
 {
@@ -21,7 +22,12 @@ namespace SamMiller.Mumba.Controllers
         private MumbaContext _context;
 
         private UserManager<AppUser> _userManager;
-
+        
+        /// <summary>
+        /// Initializes the private value _context
+        /// </summary>
+        /// <param name="context">The context of the user</param>
+        /// <param name="userManager">The authorization middleware</param>
         public BoardsController(MumbaContext context, UserManager<AppUser> userManager)
         {
             _context = context;
@@ -29,50 +35,57 @@ namespace SamMiller.Mumba.Controllers
         }
 
         /// <summary>
-        /// Gets all boards
+        /// Gets all boards belonging to the currently logged in user
         /// </summary>
-        /// <returns>A list of all technicians</returns>
+        /// <returns>The view of the users boards</returns>
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            string uId =  _userManager.GetUserAsync(User).Id.ToString();
-            var Boards = await _context.Boards.Where(Board => Board.UserId == uId ).ToListAsync();
+            string uId = _userManager.GetUserAsync(User).Id.ToString();
+            var Boards = await _context.Boards.ToListAsync();
             return View(Boards);
         }
+
         /// <summary>
         /// Opens a board
         /// </summary>
-        /// <returns></returns>
+        /// <returns>View of board</returns>
         [HttpGet]
         public async Task<IActionResult> Open([FromRoute] Guid id)
         {
             var board = await _context.Boards.FindAsync(id);
             var tasks = await _context.Tasks.Where(task => task.BoardId == id.ToString()).ToListAsync();
-            return View(model: new BoardView{Tasks = tasks});
+            var t1 = await _context.Tasks.Where(task => task.BoardId == id.ToString()).Union(tasks.Where(task => task.ListNum == 1)).ToListAsync();
+            var t2 = await _context.Tasks.Where(task => task.BoardId == id.ToString()).Union(tasks.Where(task => task.ListNum == 2)).ToListAsync();
+            var t3 = await _context.Tasks.Where(task => task.BoardId == id.ToString()).Union(tasks.Where(task => task.ListNum == 3)).ToListAsync();
+
+            return View(model: new BoardView { Board = board, TaskL1 = t1, TaskL2 = t2, TaskL3 = t3 });
         }
 
         /// <summary>
         /// Serves the add board page
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The view of the add board page</returns>
         [HttpGet]
         public IActionResult Add()
         {
-            return View(new Board {UserId = _userManager.GetUserAsync(User).Id.ToString()});
+            return View(new Board { UserId = _userManager.GetUserAsync(User).Id.ToString() });
         }
+
         /// <summary>
         /// Handles the creation of a new board
         /// </summary>
         /// <param name="board"> A board construct</param>
-        /// <returns></returns>
+        /// <returns> A new board </returns>
         [HttpPost]
         public async Task<IActionResult> Add([FromForm] Board board)
         {
-                        
-             _context.Boards.Add(board);
-             await _context.SaveChangesAsync();
+            _context.Boards.Add(board);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(All));
-
         }
+
+
+
     }
 }
--- Src/Models/BoardViewModels/BoardView.cs
+++ Src/Models/BoardViewModels/BoardView.cs
@@ -3,22 +3,36 @@ using SamMiller.Mumba.Models;
 
 namespace SamMiller.Mumba.Models.BoardViewModels
 {
+    /// <summary>
+    /// The model for the data required to view a board
+    /// </summary>
     public class BoardView
     {
-        /// <summary>
-        /// Gets User
-        /// </summary>
-        public AppUser user { get; set; }
 
         /// <summary>
         /// Gets board
         /// </summary>
-        public Board board {get; set;}
+        public Board Board {get; set;}
 
         /// <summary>
         /// List of Tasks associated with the Board
         /// </summary>
         /// <returns>Boards Tasks</returns>
         public IEnumerable<Task> Tasks { get; set; }
+
+        /// <summary>
+        /// List of tasks associated with this boards list 1
+        /// </summary>
+        public IEnumerable<Task> TaskL1 {get; set;}
+
+        /// <summary>
+        /// List of tasks associated with this boards list 2
+        /// </summary>
+        public IEnumerable<Task> TaskL2 {get; set;}
+        
+        /// <summary>
+        /// List of tasks associated with this boards list 3
+        /// </summary>
+        public IEnumerable<Task> TaskL3 {get; set;}
     }
 }
